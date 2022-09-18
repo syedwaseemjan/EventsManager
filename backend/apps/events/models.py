432c4df268dc32e292502f383e4886defb46ef24
Waseem Jan <waseem.jan@britecore.com>
2022-09-18 10:08:34 +0500

    Add basic event models

--- backend/apps/events/models.py
+++ backend/apps/events/models.py
@@ -1,3 +1,41 @@
-from django.db import models  # noqa: F401
+import uuid
 
-# Create your models here.
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class BaseModel(models.Model):
+
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+    date_added = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        abstract = True
+
+
+class Event(BaseModel):
+
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    date = models.DateTimeField()
+    creator = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    class Meta:
+        default_related_name = "events"
+        ordering = ("-date",)
+
+    def __str__(self):
+        return f"{self.title} - {self.date} - {self.creator}"
+
+
+class Participant(BaseModel):
+    event = models.ForeignKey(Event, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="going_to")
+
+    class Meta:
+        default_related_name = "participants"
+        unique_together = ("event", "user")
+
+    def __str__(self):
+        return f"{self.event} - {self.user}"
