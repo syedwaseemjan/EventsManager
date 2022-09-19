3e2eea087670537dd18f113ad3ee7ebb7688fcac
Waseem Jan <waseem.jan@britecore.com>
2022-09-19 05:49:28 +0500

    Add app name for urls

--- backend/apps/events/urls.py
+++ backend/apps/events/urls.py
@@ -3,6 +3,7 @@ from rest_framework.routers import DefaultRouter
 
 from . import views
 
+app_name = "events"
 router = DefaultRouter()
 router.register(r"events", views.EventViewSet, basename="events")
 
