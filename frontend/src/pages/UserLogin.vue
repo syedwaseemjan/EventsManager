a865bb6468058dcd498d594af4407b97a82e0dfa
Waseem Jan <waseem.jan@britecore.com>
2022-09-21 00:53:18 +0500

    Developer login page

--- frontend/src/pages/UserLogin.vue
+++ frontend/src/pages/UserLogin.vue
@@ -1,15 +1,66 @@
 <template>
-  <div class="q-pa-md">
-  </div>
+  <q-page>
+    <div class="row flex-center">
+      <div class="q-pa-md" style="min-width: 400px">
+        <q-form
+          @submit="onSubmit"
+          class="q-gutter-md"
+        >
+          <q-input
+            filled
+            type="email"
+            v-model="form.email"
+            label="Email *"
+            lazy-rules
+            :rules="[ val => val && val.length > 0 || 'Please type your email']"
+          />
+
+          <q-input
+            filled
+            type="password"
+            v-model="form.password"
+            label="Password *"
+            lazy-rules
+            :rules="[
+              val => val !== null && val !== '' || 'Please type your password'
+            ]"
+          />
+
+          <div>
+            <q-btn label="Login" type="submit" color="primary"/>
+          </div>
+        </q-form>
+      </div>
+    </div>
+  </q-page>
 </template>
 
 
 <script>
+
+import { eventStore } from 'stores/event';
+import { mapStores } from 'pinia'
+
 export default {
   name: 'UserLogin',
 
   data () {
-    return {}
+    return {
+      form: {
+        email: '',
+        password: ''
+      }
+    }
+  },
+
+  computed: {
+    ...mapStores(eventStore)
+  },
+
+  methods: {
+    onSubmit () {
+      this.eventStore.login(this.form)
+    }
   }
 }
 </script>
