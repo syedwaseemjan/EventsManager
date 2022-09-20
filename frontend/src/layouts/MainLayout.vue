d9a61b29a71bf28ad8872e4c6e4f69f53284ccb4
Waseem Jan <waseem.jan@britecore.com>
2022-09-21 00:54:02 +0500

    Handle main header links

--- frontend/src/layouts/MainLayout.vue
+++ frontend/src/layouts/MainLayout.vue
@@ -1,5 +1,5 @@
 <template>
-  <q-layout view="lHh Lpr lFf">
+  <q-layout view="hHh lpR fFf">
     <q-header elevated>
       <q-toolbar class="text-white shadow-2 rounded-borders">
         Events App
@@ -20,27 +20,47 @@
 
 <script>
 import { defineComponent, ref } from 'vue'
+import { eventStore } from 'stores/event';
+import { mapStores, mapState } from 'pinia'
 
 export default defineComponent({
   name: 'MainLayout',
 
   data () {
     return {
-      headerLinks: [
-        {label: "Home", url: '/'},
-        {label: "New Event", url: '/new-event'},
-        {label: "Logout", url: '/logout'},
-        {label: "Login", url: '/login'},
-        {label: "Signup", url: '/signup'},
-      ]
     }
+  },
+
+  computed: {
+    ...mapStores(eventStore),
+    ...mapState(eventStore, {
+      isUserAuthenticated: "isUserAuthenticated"
+    }),
+    headerLinks (){
+      let links = []
+      if (this.isUserAuthenticated) {
+        links.push(...[
+          {label: "Home", url: '/'},
+          {label: "New Event", url: '/new-event'},
+          {label: "Logout", url: '/logout'},
+        ])
+      } else {
+        links.push(...[
+          {label: "Home", url: '/'},
+          {label: "Login", url: '/login'},
+          {label: "Signup", url: '/signup'},
+        ])
+      }
+      return links;
+    }
+
   },
 
   methods: {
     navigationPreprocessor (event, tabName) {
       if (tabName == "Logout") {
         event.preventDefault()
-        this.$router.push({ path: '/' })
+        this.eventStore.logout()
       }
     }
   }
